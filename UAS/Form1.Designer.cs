6656c07c3a185caf4550abc5a783d59439d5fd14
AvKov <Inknowgt@gmail.com>
2022-01-08 21:16:34 +0700

    Score Added

---
+++ UAS/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace UAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pipeup = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.pipedown = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.pipedown2 = new System.Windows.Forms.PictureBox();
+            this.pipeup2 = new System.Windows.Forms.PictureBox();
+            this.pipedown3 = new System.Windows.Forms.PictureBox();
+            this.pipeup3 = new System.Windows.Forms.PictureBox();
+            this.pipeup4 = new System.Windows.Forms.PictureBox();
+            this.pipedown4 = new System.Windows.Forms.PictureBox();
+            this.Gameover = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gameover)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pipeup
+            // 
+            this.pipeup.Image = global::UAS.Properties.Resources.pipe_green___Copy__2_;
+            this.pipeup.Location = new System.Drawing.Point(196, -76);
+            this.pipeup.Name = "pipeup";
+            this.pipeup.Size = new System.Drawing.Size(59, 266);
+            this.pipeup.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeup.TabIndex = 4;
+            this.pipeup.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UAS.Properties.Resources.background_day;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(501, 607);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.Color.Transparent;
+            this.bird.Image = global::UAS.Properties.Resources.redbird_midflap;
+            this.bird.Location = new System.Drawing.Point(34, 337);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(58, 48);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 1;
+            this.bird.TabStop = false;
+            // 
+            // pipedown
+            // 
+            this.pipedown.BackColor = System.Drawing.Color.Transparent;
+            this.pipedown.Image = ((System.Drawing.Image)(resources.GetObject("pipedown.Image")));
+            this.pipedown.Location = new System.Drawing.Point(196, 392);
+            this.pipedown.Name = "pipedown";
+            this.pipedown.Size = new System.Drawing.Size(59, 266);
+            this.pipedown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipedown.TabIndex = 2;
+            this.pipedown.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(288, -72);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(0, 0);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ground
+            // 
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(-1, 524);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(501, 112);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 5;
+            this.ground.TabStop = false;
+            // 
+            // pipedown2
+            // 
+            this.pipedown2.BackColor = System.Drawing.Color.Transparent;
+            this.pipedown2.Image = ((System.Drawing.Image)(resources.GetObject("pipedown2.Image")));
+            this.pipedown2.Location = new System.Drawing.Point(349, 337);
+            this.pipedown2.Name = "pipedown2";
+            this.pipedown2.Size = new System.Drawing.Size(59, 266);
+            this.pipedown2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipedown2.TabIndex = 6;
+            this.pipedown2.TabStop = false;
+            // 
+            // pipeup2
+            // 
+            this.pipeup2.Image = global::UAS.Properties.Resources.pipe_green___Copy__2_;
+            this.pipeup2.Location = new System.Drawing.Point(349, -92);
+            this.pipeup2.Name = "pipeup2";
+            this.pipeup2.Size = new System.Drawing.Size(59, 266);
+            this.pipeup2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeup2.TabIndex = 7;
+            this.pipeup2.TabStop = false;
+            // 
+            // pipedown3
+            // 
+            this.pipedown3.BackColor = System.Drawing.Color.Transparent;
+            this.pipedown3.Image = ((System.Drawing.Image)(resources.GetObject("pipedown3.Image")));
+            this.pipedown3.Location = new System.Drawing.Point(506, 448);
+            this.pipedown3.Name = "pipedown3";
+            this.pipedown3.Size = new System.Drawing.Size(59, 266);
+            this.pipedown3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipedown3.TabIndex = 8;
+            this.pipedown3.TabStop = false;
+            // 
+            // pipeup3
+            // 
+            this.pipeup3.Image = global::UAS.Properties.Resources.pipe_green___Copy__2_;
+            this.pipeup3.Location = new System.Drawing.Point(506, -20);
+            this.pipeup3.Name = "pipeup3";
+            this.pipeup3.Size = new System.Drawing.Size(59, 266);
+            this.pipeup3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeup3.TabIndex = 9;
+            this.pipeup3.TabStop = false;
+            // 
+            // pipeup4
+            // 
+            this.pipeup4.Image = global::UAS.Properties.Resources.pipe_green___Copy__2_;
+            this.pipeup4.Location = new System.Drawing.Point(665, -17);
+            this.pipeup4.Name = "pipeup4";
+            this.pipeup4.Size = new System.Drawing.Size(59, 266);
+            this.pipeup4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeup4.TabIndex = 11;
+            this.pipeup4.TabStop = false;
+            // 
+            // pipedown4
+            // 
+            this.pipedown4.BackColor = System.Drawing.Color.Transparent;
+            this.pipedown4.Image = ((System.Drawing.Image)(resources.GetObject("pipedown4.Image")));
+            this.pipedown4.Location = new System.Drawing.Point(665, 392);
+            this.pipedown4.Name = "pipedown4";
+            this.pipedown4.Size = new System.Drawing.Size(59, 266);
+            this.pipedown4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipedown4.TabIndex = 10;
+            this.pipedown4.TabStop = false;
+            // 
+            // Gameover
+            // 
+            this.Gameover.BackColor = System.Drawing.Color.Transparent;
+            this.Gameover.Image = global::UAS.Properties.Resources.gameover;
+            this.Gameover.Location = new System.Drawing.Point(96, 237);
+            this.Gameover.Name = "Gameover";
+            this.Gameover.Size = new System.Drawing.Size(324, 83);
+            this.Gameover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Gameover.TabIndex = 12;
+            this.Gameover.TabStop = false;
+            this.Gameover.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("OCR A Extended", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 37);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 601);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Gameover);
+            this.Controls.Add(this.pipeup4);
+            this.Controls.Add(this.pipedown4);
+            this.Controls.Add(this.pipedown3);
+            this.Controls.Add(this.pipeup3);
+            this.Controls.Add(this.pipeup2);
+            this.Controls.Add(this.pipedown2);
+            this.Controls.Add(this.pipedown);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pipeup);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gameover)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox pipedown;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pipeup;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox pipedown2;
+        private System.Windows.Forms.PictureBox pipeup2;
+        private System.Windows.Forms.PictureBox pipedown3;
+        private System.Windows.Forms.PictureBox pipeup3;
+        private System.Windows.Forms.PictureBox pipeup4;
+        private System.Windows.Forms.PictureBox pipedown4;
+        private System.Windows.Forms.PictureBox Gameover;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ UAS/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UAS
+{
+    public partial class Form1 : Form
+    {
+
+        bool jumping = false;
+        int gravity = 5;
+        int speed = 5;
+        int scoretotal = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+            // Transparent Picturebox
+            pictureBox1.Controls.Add(bird);
+            pictureBox1.Controls.Add(Gameover);
+            // Transparent Gameover
+            Gameover.Location = new Point(96, 237);
+            Gameover.BackColor = Color.Transparent;
+            // Transparent Bird
+            bird.BackColor = Color.Transparent;
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                jumping = true;
+                gravity = 15;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                jumping = false;
+                gravity = -15;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pipedown.Left -= speed;
+            pipeup.Left -= speed;
+            pipedown2.Left -= speed;
+            pipeup2.Left -= speed;
+            pipedown3.Left -= speed;
+            pipeup3.Left -= speed;
+            pipedown4.Left -= speed;
+            pipeup4.Left -= speed;
+            bird.Top += gravity;
+
+            label1.Text = scoretotal.ToString();
+            
+                
+            if (pipedown.Left < -80)
+            {
+                pipedown.Left = 500;
+                
+            }
+            else if (pipeup.Left < -80)
+            {
+                pipeup.Left = 500;
+                scoretotal++;
+            }
+            //Pipe 2
+            else if (pipedown2.Left < -80)
+            {
+                pipedown2.Left = 500;
+                
+            }
+            else if (pipeup2.Left < -80)
+            {
+                pipeup2.Left = 500;
+                scoretotal++;
+            }
+            //Pipe 3
+            else if (pipedown3.Left < -80)
+            {
+                pipedown3.Left = 500;
+                
+            }
+            else if (pipeup3.Left < -80)
+            {
+                pipeup3.Left = 500;
+                scoretotal++;
+            }
+            //Pipe 4
+            else if (pipedown4.Left < -80)
+            {
+                pipedown4.Left = 500;
+                
+            }
+            else if (pipeup4.Left < -80)
+            {
+                pipeup4.Left = 500;
+                scoretotal++;
+            }
+
+            // kalo nabrak pipe 1
+            if (bird.Bounds.IntersectsWith(pipeup.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+            else if (bird.Bounds.IntersectsWith(pipedown.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+            // kalo nabrak pipe 2
+            else if (bird.Bounds.IntersectsWith(pipeup2.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+            else if (bird.Bounds.IntersectsWith(pipedown2.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+            // kalo nabrak pipe 3
+            else if (bird.Bounds.IntersectsWith(pipeup3.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+            else if (bird.Bounds.IntersectsWith(pipedown3.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+            // kalo nabrak pipe 4
+            else if (bird.Bounds.IntersectsWith(pipeup4.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+            else if (bird.Bounds.IntersectsWith(pipedown4.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+            // kalo nabrak ground
+            else if (bird.Bounds.IntersectsWith(ground.Bounds))
+            {
+                Gameover.Visible = true;
+                timer1.Stop();
+            }
+        }
+    }
+}
